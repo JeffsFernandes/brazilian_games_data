bb411312d498a0caaa17026532d1656b8073887f
Jefferson Fernandes <jeffs.fernandes@gmail.com>
2016-07-06 21:32:54 -0300

    Selecionando todas tabelas de game na pagina

--- play.rb
+++ play.rb
@@ -1,11 +1,16 @@
 puts 'play the beautiful game'
 require 'selenium-webdriver'
 
+def process_game
+  # 'table tbody tr td font b'
+end
+
 def play
   data = []
   @driver = Selenium::WebDriver.for :firefox
   @driver.navigate.to 'http://localhost:8000/brazil2016.htm'
-  puts @driver.find_element(:css, 'table tbody tr td font b').text
+  game_tables = @driver.find_elements(:css, 'body > table')
+  game_tables.each { |game| process_game(game) }
   @driver.quit
   return data
 end
