bed47b880ba578ca6c3c802d7d3ec96d31c5f779
Jefferson Fernandes <jeffs.fernandes@gmail.com>
2016-07-12 01:18:12 -0300

    Separando game de game_table e montanto estrutura de build e save game

--- play.rb
+++ play.rb
@@ -1,9 +1,17 @@
 puts 'play the beautiful game'
 require 'selenium-webdriver'
 
-def process_game game
+def process_game game_table
+  save(build_game(game_table))
+end
+
+def save game
+  #ToDo
+end
+def build_game game_table
+  game = {}
   # raise game.inspect
-  puts game.find_element(:css, 'tbody tr td font b').text
+  puts game_table.find_element(:css, 'tbody tr td font b').text
 end
 
 def play
@@ -11,7 +19,7 @@ def play
   @driver = Selenium::WebDriver.for :firefox
   @driver.navigate.to 'http://localhost:8000/brazil2016.htm'
   game_tables = @driver.find_elements(:css, 'body > table')
-  game_tables.each { |game| process_game(game) }
+  game_tables.each { |game_table| process_game(game_table) }
   @driver.quit
   return data
 end
